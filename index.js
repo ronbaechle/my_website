bf460e9a15ec5f101a2fcabce3a428b2f46458b7
ronbaechle <ronbaechle@gmail.com>
2025-10-07 20:27:07 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+//console.log("This is a test");
+//window.alert("Rembot!");
+//this is a comment
+
+document.getElementById("myh1").textContent = "hello";
+document.getElementById("myp1").textContent = "This is a paragraph";
